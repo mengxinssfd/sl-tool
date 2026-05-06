139d3ee09e390cb072a6b3dc42bb435d8c280e02
DYH_HP <xinzon32177@163.com>
2026-05-06 12:13:53 +0800

    chore: 修复electron/main/index.ts不识别import.meta.env的问题

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -2,7 +2,6 @@
 interface ImportMetaEnv {
   readonly VITE_API_KEY: string;
   readonly VITE_SIGNAL_OPEN_DIRECTORY_DIALOG: string;
-  readonly VITE_SIGNAL_OPEN_DIRECTORY_DIALOG: string;
 }
 
 interface ImportMeta {
